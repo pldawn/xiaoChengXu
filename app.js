d6b83355dcd0b000bbe035096b3bd87d3bb87701
pldawn <zhangwentaosnake@163.com>
2018-02-24 11:00:13 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+App({
+  onLaunch: function () {
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success: res => {
+              this.globalData.userInfo = res.userInfo
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+  globalData: {
+    userInfo: null,
+    pressId: null,
+    pressKey: null,
+    newContent: null,
+    addressBook: null
+  }
+})
