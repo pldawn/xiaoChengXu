afe18e9fd3babc3e2893869adf58693fb67ee1a6
libing <makeup1123@163.com>
2018-02-24 18:01:09 +0800

    fix

---
+++ pages/contact/index.js
@@ -0,0 +1,70 @@
+// pages/contact/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    addressBook: [
+      { name: "张文涛", mobile: "13520946941", status: "Boss直聘", city: "北京", update_time:"2018.1.2"},
+      { name: "彭琳", mobile: "17301860049", status: "上海财经大学", city: "上海", update_time: "2018.1.2"},
+      { name: "李卓航", mobile: "13632412301", status: "中山大学附属第二医院泌尿科", city: "广州", update_time: "2018.1.2"}
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
